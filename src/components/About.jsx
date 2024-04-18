81f4a93c7caa5b2d3e368298c582224d2bb37e30
Ayo-kunle <edwardsayokunle@gmail.com>
2024-04-18 18:22:07 -0400

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled software developer with extensive experience in TypeScript
+        and JavaScript, specializing in frameworks like React, Node.js, and
+        Three.js. With a background as an IT business analyst, I bring a unique
+        blend of technical expertise and business acumen to projects. <br />
+        I thrive on collaborating closely with clients to understand their needs
+        thoroughly. By leveraging my analytical skills, I translate complex
+        requirements into efficient, scalable, and user-friendly solutions that
+        address real-world challenges. <br />
+        As a quick learner, I adapt swiftly to new technologies and
+        methodologies, ensuring that I stay at the forefront of industry trends.
+        Let's work together to bring your ideas to life and drive your business
+        forward!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
